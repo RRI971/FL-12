b105101e076f53bb08c18a216300f44c04cc0946
RRI971 <rri2@yandex.ru>
2019-12-24 23:52:08 +0200

    Commit third FL-12.Homework 6

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,3 +1,13 @@
-var a = prompt("Enter a:", 1);
-var b = prompt("Enter b:", 1);
-var c = prompt("Enter c:", 1);
+var a = prompt("Enter a:", 0);
+var b = prompt("Enter b:", 0);
+var c = prompt("Enter c:", 0);
+
+if((a*a + b*b == c*c) || (a*a + c*c == b*b) || (b* + c*c == a*a)) {
+    alert("Right triangle");
+} else if(( a == b ) && ( a==c )) {
+    alert("Equilateral triangle");
+} else if(( a == b ) || ( b ==c )||( a==c )) {
+    alert("Isosceles triangle");
+} else {
+    alert("Ordinary triangle");
+}
